4524ede4cbafce85a674e8e855e218c6d4a0ad52
Ranjit2000 <r2anjit2000@gmail.com>
2025-08-23 16:05:53 +0530

    Workflow setup

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
-import workflowRouter from "./routes/workflow.routes.js";
+//import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.use("/api/v1/workflows", workflowRouter);
+//app.use("/api/v1/workflows", workflowRouter);
 
 
 
